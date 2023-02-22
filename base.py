744c3567a9330678aaa3f14aa9ddf349f1f9742c
Kravandir <kravandir@gmail.com>
2023-02-22 18:39:16 +0400

    Почищено, упрощено для понимания

--- base.py
+++ base.py
@@ -9,10 +9,11 @@ class Import(BS):
 
 
 def int_to_bool(i):
+    '''Из нуля получаем строку True'''
     return str(bool(i))
 
 
-def get_type(str:str, item:Optional[dict]=None):
+def get_type(str:str|list, item:Optional[dict]=None):
     match str:
         case 'string':
             return 'str'
@@ -45,18 +46,20 @@ def get_type(str:str, item:Optional[dict]=None):
 
 
 def get_file_path(file:str,json:str) -> str|None:
+    '''Получаем путь к файлу, в случае ненахода None'''
     if os.path.exists(file+'/'+json):
         return 'vk_api_schema/' + file + '/'+ json
     else: return None
 
 
-def gen_imports(i:dict):
-    path = i['file_path']
-    imports =', '.join(i['imports'])
+def gen_import(a:Import):
+    path = a.file_path
+    imports =', '.join(a.imports)
     return 'from ..' + path + ' import ' + imports
 
 
 def clean_dir(list:list) -> list[str]:
+    '''Чистим от НЕ директорий'''
     for i in list[:]:
         if not os.path.isdir(i):
             list.remove(i)
@@ -64,11 +67,13 @@ def clean_dir(list:list) -> list[str]:
     
 
 def get_class_name(orig:str) -> str:
+    '''base_shit -> BaseShit'''
     name = orig.split('_')
     return name[0][0].upper() + name[0][1:] + ''.join(i.title() for i in name[1:])
 
 
-def get_files() -> list:
+def get_dirs() -> list:
+    '''Получаем директории'''
     work_dirs = os.listdir('vk_api_schema')
     return clean_dir(work_dirs)
 
--- methods.py
+++ methods.py
@@ -1,11 +1,9 @@
 import json
-from base import Import, get_file_path, get_files, gen_imports, get_type, int_to_bool
+from base import Import, get_file_path, get_dirs, gen_import, get_type, int_to_bool
 from pydantic import BaseModel as BS
 from typing import Optional
 from loguru import logger
 
-# KAL = ['.git', 'addresses', 'audio', 'base', 'callback', 'calls', 'client', 'comment', 
-#        'events', 'tasks', 'stickers', 'podcast', 'owner', 'oauth', 'link']
 BAD_NAMES = ['global', 'extended']
 
 class Parameter(BS):
@@ -23,17 +21,20 @@ class Parameter(BS):
 
 
     def __str__(self):
+        '''Получаем примерно такое:
+            peremen:str=\"sosi\"'''
         string = f'{self.name}:{self.type}'
         if self.default != None: 
             if self.type == 'int' or self.type == 'bool':
                 string = string+f'={self.default}'
-            # elif type == 'str':
             else:
                 string = string+f'="{self.default}"'
         return string
 
 
 def sort_parameters(p:Parameter):
+    '''Функция для list.sort
+    Сортирует по наличию дефолтного значения'''
     return p.default != None
 
 
@@ -49,6 +50,12 @@ class Method(BS):
 
 
     def __str__(self) -> str:
+        '''TODO: ВОЗВРАЩАТЬ НЕ СЫРОЙ А ОБРАБОТАННЫЙ ОТВЕТ
+        Получаем примерно такое:
+            async def sosi(self, xyi:str, times:int=1):
+                r = await self.method(\"xyi.sosi\", **locals())
+                return r'''
+
         if self.parameters == []: params = ''
         else: params = ', ' + get_params(self.parameters)
         if self.description == None: description = ''
@@ -78,6 +85,7 @@ def get_params(params:list[Parameter]):
 
 @logger.catch
 def gen_file(file_name:str):
+    '''Тут пиздец'''
     with open(file=file_name) as f:
         scheme = json.load(f)
     
@@ -92,18 +100,17 @@ def gen_file(file_name:str):
                 params.append(get_parameter(i))
         method = Method(name=name, parameters=params, class_name=class_name, description=description)
         r = r + method.__str__() + '\n\n'
-        # print(method.__str__(), '\n\t\t\n\n')
     return r
 
 
 def main():
     depencies = [Import(file_path='base', imports=['BaseMethod'])]
-    for f in get_files():
+    for f in get_dirs():
         file = get_file_path(f, 'methods.json')
         if file == None: continue
         text = gen_file(file)
         for d in depencies:
-            text = gen_imports(d.__dict__) + '\n\n' + text
+            text = gen_import(d) + '\n\n' + text
         with open(file.replace('.json', '.py'), 'w') as file:
             file.write(text)
     
